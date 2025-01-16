4a606e705b7d985f71380286bbd9a27279a3d9bd
raylee <shreyali.d.10@gmail.com>
2025-01-16 11:06:21 +0530

    Array folder

---
+++ Arrays/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+
+public class Main {
+    public Static int[] removedup(int[] arr, int a){
+        arrays.sort(arr)
+    }
+    public static void main(String[] args) {
+
+
+
+    }
+}
